06dbcacd26707ccaf421605f63813b804861c1a9
shubham3000 <shubhamsadhu02@gmail.com>
2024-04-14 20:48:49 +0530

    clippath updated

--- src/Hero7.jsx
+++ src/Hero7.jsx
@@ -13,25 +13,29 @@ export default function Hero7() {
       <section className="bg-white flex justify-center py-16 px-8">
         <div className="grid max-w-screen-xl mx-auto gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 relative">
           <div className="xl:col-span-6 lg:col-span-6">
-            <div className="w-[325px] h-[310px] xl:w-[588px] xl:h-[455px] bg-no-repeat p-10 flex flex-col justify-center mx-3" style={{ backgroundImage: `url(${window.innerWidth <= 1024 ? ClipPathMb : ClipPath})` }}>
-              <div className='flex justify-between items-center w-4/5'>
-                <h2 className='text-2xl xl:text-5xl font-bold text-[#001856]'>Our <br /> <span className='text-4xl xl:text-7xl font-bold'>Vision</span></h2>
-                <img src={Eye} alt="Eye" className=' w-10 h-10 xl:w-20 xl:h-20' />
-              </div>
-              <div className=" mt-10">
-                <p className='text-[#001856] text-base xl:text-xl font-normal'>To redefine the logistics landscape by providing a complete logistics solution that surpasses industry standards. We aim to set new benchmarks in safety, dependability, and quality service, becoming the most trusted logistics player in the industry.</p>
-              </div>
+            <div className="w-[320px] h-[307px] xl:w-[588px] xl:h-[455px] bg-black flex items-center justify-center mx-3" style={{ clipPath: "polygon(0 0, 60% 0, 100% 40%, 100% 100%, 0 100%, 0% 50%)" }}>
+              <span class="border-span w-[318px] h-[305px] xl:w-[584px] xl:h-[452px] bg-white p-4 md:p-10  flex flex-col justify-center" style={{ clipPath: "polygon(0 0, 60% 0, 100% 40%, 100% 100%, 0 100%, 0% 50%)" }}>
+                <div className='flex justify-between items-center w-4/5'>
+                  <h2 className='text-2xl xl:text-5xl font-bold text-[#001856]'>Our <br /> <span className='text-4xl xl:text-7xl font-bold'>Vision</span></h2>
+                  <img src={Eye} alt="Eye" className=' w-10 h-10 xl:w-20 xl:h-20' />
+                </div>
+                <div className=" mt-10">
+                  <p className='text-[#001856] text-sm xl:text-xl font-normal'>To redefine the logistics landscape by providing a complete logistics solution that surpasses industry standards. We aim to set new benchmarks in safety, dependability, and quality service, becoming the most trusted logistics player in the industry.</p>
+                </div>
+              </span>
             </div>
           </div>
           <div className="xl:col-span-6 lg:col-span-6 lg:absolute lg:right-0 lg:top-[300px]">
-            <div className=" w-[325px] h-[310px] xl:w-[588px] xl:h-[455px] bg-no-repeat p-10 flex flex-col justify-center mx-3" style={{ backgroundImage: `url(${window.innerWidth <= 1024 ? ClipPathMb : ClipPath})` }}>
-              <div className='flex justify-between items-center w-4/5'>
-                <h2 className=' text-2xl xl:text-5xl font-bold text-[#001856]'>Our <br /> <span className='text-4xl xl:text-7xl font-bold'>Values</span></h2>
-                <img src={Badge} alt="Badge" className=' w-10 h-10 xl:w-20 xl:h-20' />
-              </div>
-              <div className=" mt-10">
-                <p className='text-[#001856] text-base xl:text-xl font-normal'>We are committed to delivering excellence in every aspect of our operations. From cargo handling to transportation, we strive to exceed expectations and set a new standard for quality service.</p>
-              </div>
+            <div className="w-[320px] h-[307px] xl:w-[588px] xl:h-[455px] bg-black flex items-center justify-center mx-3" style={{ clipPath: "polygon(0 0, 60% 0, 100% 40%, 100% 100%, 0 100%, 0% 50%)" }}>
+              <span class="border-span w-[318px] h-[305px] xl:w-[584px] xl:h-[452px] bg-white p-4 md:p-10 flex flex-col justify-center" style={{ clipPath: "polygon(0 0, 60% 0, 100% 40%, 100% 100%, 0 100%, 0% 50%)" }}>
+                <div className='flex justify-between items-center w-4/5'>
+                  <h2 className=' text-2xl xl:text-5xl font-bold text-[#001856]'>Our <br /> <span className='text-4xl xl:text-7xl font-bold'>Values</span></h2>
+                  <img src={Badge} alt="Badge" className=' w-10 h-10 xl:w-20 xl:h-20' />
+                </div>
+                <div className=" mt-10">
+                  <p className='text-[#001856] text-sm xl:text-xl font-normal'>We are committed to delivering excellence in every aspect of our operations. From cargo handling to transportation, we strive to exceed expectations and set a new standard for quality service.</p>
+                </div>
+              </span>
             </div>
           </div>
 
